b893cb6371cf994bfd7a69162a6f753f5195303e
Ben Pearce <benpearce9@live.co.uk>
2021-04-24 17:38:28 +0100

    feat(gallery.controller.ts): add getOne and getAll controllers methods

--- server/src/gallery/gallery.controller.ts
+++ server/src/gallery/gallery.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import artData from './art-data';
+import { ArtPiece } from './art-piece.interface';
 
 @Controller('gallery')
-export class GalleryController {}
+export class GalleryController {
+  @Get()
+  getAll(): ArtPiece[] {
+    return artData;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string): ArtPiece {
+    return artData[id];
+  }
+}
