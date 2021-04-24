6cbbacf3f4a028655928d7b9569bbaf98acb26e2
Ben Pearce <benpearce9@live.co.uk>
2021-04-24 17:29:12 +0100

    feat(main.ts): enable cors, add console log url message

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({ origin: 'http://localhost:3000' });
+  await app.listen(4000, () =>
+    console.log('Server listening on http:/localhost:4000'),
+  );
 }
 bootstrap();
