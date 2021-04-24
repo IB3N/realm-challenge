725529d25eae492e96326842e80759a905fb747f
Ben Pearce <benpearce9@live.co.uk>
2021-04-24 18:10:42 +0100

    feat(App.tsx): add safe area provider at root of app

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </SafeAreaProvider>
   );
 }
 
