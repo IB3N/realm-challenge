ba8e475250185589fac8e0b449a672caad220466
Ben Pearce <benpearce9@live.co.uk>
2021-04-24 19:05:36 +0100

    feat(HomeScreen.tsx): basic setup

--- client/screens/HomeScreen.tsx
+++ client/screens/HomeScreen.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
-  return <Text>Hello Home</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Realm Art Gallery</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  welcome: {
+    alignSelf: 'center',
+  },
+});
