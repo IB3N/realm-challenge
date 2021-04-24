6b942e2384c90c891d281beb39ade19590a0c56d
Ben Pearce <benpearce9@live.co.uk>
2021-04-24 17:44:11 +0100

    chore(app.module.ts): remove unneccessary import and providers module props

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { GalleryController } from './gallery/gallery.controller';
 
 @Module({
-  imports: [],
   controllers: [GalleryController],
-  providers: [],
 })
 export class AppModule {}
