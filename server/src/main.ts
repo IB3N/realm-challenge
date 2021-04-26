3454c4c542ccebc10cfceec7dbcefb4582a6574a
Ben Pearce <benpearce9@live.co.uk>
2021-04-26 13:14:09 +0100

    feat(heroku deploy server): change package.json and Procfile to deploy on heroku

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,10 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:19006', 'http://192.168.1.6:19000'],
-  });
-  await app.listen(4000, () =>
+  app.enableCors();
+  await app.listen(process.env.PORT || 4000, () =>
     console.log('Server listening on http:/localhost:4000'),
   );
 }
